873b6c766a3c1c4583da6f4af6467ee64e47da37
Hysenburg <freedomfighterit588@gmail.com>
2025-09-11 13:25:02 +0600

    Add script to generate list and tuple from comma-separated input

---
+++ createLIstandTuple.py
@@ -0,0 +1,17 @@
+# writr a code that accepts  a sequence of comma separated numbers 
+# from the user and generated a list and a tuple of those number 
+
+values = input("Input some comma-separated numbers: ")
+
+# Split the 'values' string into a list using commas as separators and store it in the 'list' variable
+list = values.split(",")
+
+# Convert the 'list' into a tuple and store it in the 'tuple' variable
+tuple = tuple(list)
+
+# Print the list
+print('List : ', list)
+
+# Print the tuple
+print('Tuple : ', tuple)
+
