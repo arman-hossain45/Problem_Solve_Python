8273701644f5dc6570119711c73c3798fc51717a
Hysenburg <freedomfighterit588@gmail.com>
2025-09-20 00:34:05 +0600

    bublle sort

---
+++ DSA/bubbleSort.py
@@ -0,0 +1,16 @@
+#bublle sort algoritm 
+
+def sort(num):
+    n = len(num)   # get length of the list
+    # loop from n-2 down to 0 with step -2
+    for i in range(n-2, -1, -1):  
+        for j in range(0, i+1):
+            if num[j] > num[j+1]:
+                # swap
+                num[j], num[j+1] = num[j+1], num[j]
+    return num   # return sorted list
+
+
+# Example
+arr = [5, 2, 9, 1, 7, 6]
+print(sort(arr))
