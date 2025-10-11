b6f2acbcfe250e0e9835491db10462db360b5567
Hysenburg <freedomfighterit588@gmail.com>
2025-10-11 20:32:44 +0600

    even odd

--- DSA/print5number.py
+++ DSA/print5number.py
@@ -1,4 +1,12 @@
-n=int(input("Enter a number :"))
+#  n=int(input("Enter a number :"))
 
-for i in range(n):
-    print(i)
+# for i in range(1,n+1):
+#     print(i)
+
+n=int(input("enter a number "))
+
+# for i in range(2,n+1,2):
+#     print(i)
+for i in range(1,n+1):
+    if i%2==0:
+        print(i)
