b4cc460ab4273c45eedee9e6d3561fcbea576609
Hysenburg <freedomfighterit588@gmail.com>
2025-10-11 21:00:28 +0600

    even,odd,pos and neg

--- DSA/print5number.py
+++ DSA/print5number.py
@@ -3,10 +3,10 @@
 # for i in range(1,n+1):
 #     print(i)
 
-n=int(input("enter a number "))
+# n=int(input("enter a number "))
 
 # for i in range(2,n+1,2):
 #     print(i)
-for i in range(1,n+1):
-    if i%2==0:
-        print(i)
+# for i in range(1,n+1):
+#     if i%2==0:
+#         print(i)
---
+++ DSA/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+n=int(input())
+inp=input()
+numbers=inp.split()
+print(numbers)
